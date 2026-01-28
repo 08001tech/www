77ddf79c398ae2024818ca6aa39f689ce5b5c427
René <renejfc@gmail.com>
2026-01-28 02:35:08 +0100

    remove next-themes, unused components, and hardcode dark theme

--- apps/web/src/components/layout/providers.tsx
+++ apps/web/src/components/layout/providers.tsx
@@ -1,18 +1,3 @@
-"use client";
-
-import { Toaster } from "../ui/sonner";
-import { ThemeProvider } from "./theme-provider";
-
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      disableTransitionOnChange
-      enableSystem
-    >
-      {children}
-      <Toaster richColors />
-    </ThemeProvider>
-  );
+  return <>{children}</>;
 }
